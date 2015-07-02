564484eaa1cf63237aca70f05c4aff78aed738f1
alef <alef@alef-nb>
2015-07-02 14:07:25 +0700

    02072015

--- public_html/js/index.js
+++ public_html/js/index.js
@@ -1,17 +1,31 @@
+
+
 function vcenter(thisClassName) {
-    $(thisClassName).css({
-        'top': '50%',
-        'margin-top': -$(thisClassName).height() / 2
-    });
+    var elementParentClass = $('.' + thisClassName).parent().attr('class');
+    var elementCount = document.getElementsByClassName(elementParentClass).length;
+    var elementClassName = '';
+    for (i = 1; i < elementCount + 1; i++) {
+        elementClassName = '.' + elementParentClass + ':nth-child(' + i + ') > .' + thisClassName;
+        $(elementClassName).css({
+            'top': '50%',
+            'margin-top': -$(elementClassName).height() / 2
+        });
+    }
 }
 
 function hcenter(thisClassName) {
-    $(thisClassName).css({
-        'left': '50%',
-        'margin-left': -$(thisClassName).width() / 2
-    });
+    var elementParentClass = $('.' + thisClassName).parent().attr('class');
+    var elementCount = document.getElementsByClassName(elementParentClass).length;
+    var elementClassName = '';
+    for (i = 1; i < elementCount + 1; i++) {
+        elementClassName = '.' + elementParentClass + ':nth-child(' + i + ') > .' + thisClassName;
+        $(elementClassName).css({
+            'left': '50%',
+            'margin-left': -$(elementClassName).width() / 2
+        });
+    }
 }
 
 $(document).ready(function () {
-    vcenter('.content-item');
+    vcenter('content-item');
 });
